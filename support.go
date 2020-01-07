2f8b76ead689b92cf10aeb322685e5a354efbe8e
Jon Palmisciano <jonpalmisc@gmail.com>
2020-01-07 07:42:02 -0500

    Version number bump - 0.2.1 -> 0.2.2

--- support.go
+++ support.go
@@ -4,7 +4,7 @@ import "strings"
 
 const (
 	ProgramName    string = "Atto"
-	ProgramVersion string = "0.2.1"
+	ProgramVersion string = "0.2.2"
 	ProgramAuthor  string = "Jon Palmisciano <jonpalmisc@gmail.com>"
 )
 
