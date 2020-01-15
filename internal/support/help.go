0feb42abdc21ea21b43ea7898cf8aef0577b181d
Jon Palmisciano <jonpalmisc@gmail.com>
2020-01-15 18:34:54 -0500

    Fixed bug preventing underscores from being typed

--- internal/support/help.go
+++ internal/support/help.go
@@ -1,6 +1,6 @@
 package support
 
-const AttoVersion string = "0.5.5"
+const AttoVersion string = "0.5.6"
 
 var HelpMessage = []string{
 	"Atto - A lightweight, opinionated text editor written in Go.",
